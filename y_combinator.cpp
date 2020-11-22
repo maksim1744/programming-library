b29e2941f04b1f59809ae3b06f904a2114f77471
Maksim Gorokhovskiy <magor1744@gmail.com>
2020-11-23 01:22:56 +0300

    y_combinator

---
+++ y_combinator.cpp
@@ -0,0 +1,19 @@
+template<class Fun>
+class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T>
+    explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+
+    template<class ...Args>
+    decltype(auto) operator()(Args &&...args) {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
+};
+template<class Fun>
+decltype(auto) y_combinator(Fun &&fun) {
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+// auto gcd = std::y_combinator([](auto gcd, int a, int b) -> int {
+//     return b == 0 ? a : gcd(b, a % b);
+// });
