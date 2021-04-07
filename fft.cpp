fc3d206b7ae0ec048b6a237c72460fc79be2a0d8
Maksim Gorokhovskiy <magor1744@gmail.com>
2021-04-07 23:37:31 +0300

    shorter formulas

--- fft.cpp
+++ fft.cpp
@@ -13,12 +13,10 @@ struct Complex {
     }
 };
 
-void     operator *= (      Complex &a, const Complex &b) { a = Complex(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x); }
 Complex  operator *  (const Complex &a, const Complex &b) { return Complex(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x); }
 void     operator /= (      Complex &a, int n)            { a.x /= n; a.y /= n; }
-void     operator += (      Complex &a, const Complex &b) { a.x += b.x; a.y += b.y; }
+Complex  operator /  (const Complex &a, int n)            { return Complex(a.x / n, a.y / n); }
 Complex  operator +  (const Complex &a, const Complex &b) { return Complex(a.x + b.x, a.y + b.y); }
-void     operator -= (      Complex &a, const Complex &b) { a.x -= b.x; a.y -= b.y; }
 Complex  operator -  (const Complex &a, const Complex &b) { return Complex(a.x - b.x, a.y - b.y); }
 
 string   to_string   (            const Complex &x) { return (string)"(" + std::to_string(x.x) + ", " + std::to_string(x.y) + ")"; };
@@ -125,12 +123,11 @@ vector<long long> multiply(const vector<int> &a, const vector<int> &b) {
 
         int j = (n - i) & (n - 1);
 
-        dbl a1i = ( buf1[i].x + buf1[j].x) / 2;
-        dbl b2i = (-buf1[i].x + buf1[j].x) / 2;
-        dbl a2i = ( buf1[i].y - buf1[j].y) / 2;
-        dbl b1i = ( buf1[i].y + buf1[j].y) / 2;
-        buf1[i] = buf1[j] = Complex(a1i * b1i - a2i * b2i, a1i * b2i + a2i * b1i);
-        buf1[j].y *= -1;
+        auto v = (buf1[i] + buf1[j].conj()) * (buf1[i] - buf1[j].conj()) / 4;
+        swap(v.x, v.y);
+
+        buf1[i] = v.conj();
+        buf1[j] = v;
     }
 
     fft_internal(buf1, 0, n, true);
@@ -163,26 +160,21 @@ vector<int> multiply_mod(const vector<int> &a, const vector<int> &b, int mod) {
     for (int i = 0; i <= (n >> 1); ++i) {
         int j = (n - i) & (n - 1);
 
-        dbl as1i = ( buf1[i].x + buf1[j].x) / 2;
-        dbl al2i = (-buf1[i].x + buf1[j].x) / 2;
-        dbl as2i = ( buf1[i].y - buf1[j].y) / 2;
-        dbl al1i = ( buf1[i].y + buf1[j].y) / 2;
+        Complex as = (buf1[i] + buf1[j].conj()) / 2;
+        Complex bs = (buf2[i] + buf2[j].conj()) / 2;
+        Complex al = (buf1[i] - buf1[j].conj()) / 2;
+        Complex bl = (buf2[i] - buf2[j].conj()) / 2;
 
-        dbl bs1i = ( buf2[i].x + buf2[j].x) / 2;
-        dbl bl2i = (-buf2[i].x + buf2[j].x) / 2;
-        dbl bs2i = ( buf2[i].y - buf2[j].y) / 2;
-        dbl bl1i = ( buf2[i].y + buf2[j].y) / 2;
+        Complex asbs = as * bs;
+        Complex albs = al * bs;
+        Complex asbl = as * bl; swap(asbl.x, asbl.y);
+        Complex albl = al * bl; swap(albl.x, albl.y);
 
-        Complex asbs(as1i * bs1i - as2i * bs2i, as1i * bs2i + as2i * bs1i);
-        Complex asbl(as1i * bl1i - as2i * bl2i, as1i * bl2i + as2i * bl1i);
-        Complex albs(al1i * bs1i - al2i * bs2i, al1i * bs2i + al2i * bs1i);
-        Complex albl(al1i * bl1i - al2i * bl2i, al1i * bl2i + al2i * bl1i);
+        buf1[i] = asbs + albl.conj();
+        buf1[j] = asbs.conj() - albl;
 
-        buf1[i] = asbs + Complex(-albl.y, albl.x);
-        buf1[j] = asbs.conj() + Complex(albl.y, albl.x);
-
-        buf2[i] = asbl + Complex(-albs.y, albs.x);
-        buf2[j] = asbl.conj() + Complex(albs.y, albs.x);
+        buf2[i] = asbl.conj() + albs;
+        buf2[j] = asbl - albs.conj();
     }
 
     fft_internal(buf1, 0, n, true);
