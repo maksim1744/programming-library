b79fab61c64013a1553b6ff3bfee8ba298b1669d
Maksim Gorokhovskiy <magor1744@gmail.com>
2021-04-19 01:37:55 +0300

    better rational

--- rational.cpp
+++ rational.cpp
@@ -3,6 +3,9 @@ struct Rational {
     T a, b;
     Rational(T a = 0, T b = 1) : a(a), b(b) { norm(); }
 
+    T floor() const { T res = a / b; if (a < 0 && res != b * res) --res; return res; }
+    T ceil()  const { T res = a / b; if (a > 0 && res != b * res) ++res; return res; }
+
     friend Rational<T>& operator += (      Rational<T> &n, const Rational<T> &m) { return n = Rational(n.a * m.b + n.b * m.a, n.b * m.b); }
     friend Rational<T>  operator +  (const Rational<T> &n, const Rational<T> &m) { auto res = n; return res += m; }
     friend Rational<T>& operator -= (      Rational<T> &n, const Rational<T> &m) { return n = Rational(n.a * m.b - n.b * m.a, n.b * m.b); }
@@ -13,8 +16,34 @@ struct Rational {
     friend Rational<T>& operator /= (      Rational<T> &n, const Rational<T> &m) { return n = Rational(n.a * m.b, n.b * m.a); }
     friend Rational<T>  operator /  (const Rational<T> &n, const Rational<T> &m) { auto res = n; return res /= m; }
 
+    // doesn't overflow
+    friend bool operator <  (Rational<T> n, Rational<T> m) {
+        T a = n.floor(), b = m.floor(); if (a != b) return a < b;
+        n -= a; m -= b;
+        while (true) {
+            if (n.b == m.b) return n.a < m.a;
+            if (n.a == 0) return true; else if (m.a == 0) return false;
+            swap(n, m); swap(n.a, n.b); swap(m.a, m.b);
+            n.a %= n.b; m.a %= m.b;
+        }
+        return false;
+    }
+    // maybe faster?
+    // friend bool operator <  (const Rational<T> &n, const Rational<T> &m) { return n.a * m.b < n.b * m.a; }
+    friend bool operator <= (const Rational<T> &n, const Rational<T> &m) { return !(m < n); }
+    friend bool operator >  (const Rational<T> &n, const Rational<T> &m) { return   m < n ; }
+    friend bool operator >= (const Rational<T> &n, const Rational<T> &m) { return !(n < m); }
+
     friend bool operator == (const Rational<T> &n, const Rational<T> &m) { return n.a == m.a && n.b == m.b; }
     friend bool operator != (const Rational<T> &n, const Rational<T> &m) { return n.a != m.a || n.b != m.b; }
+
+    static T gcd(T a, T b) {
+        while (b != 0) {
+            a %= b;
+            swap(a, b);
+        }
+        return a;
+    }
 
     void norm() {
         if (b < 0) {
