f984d94c4fc2aaaf41801eb6c4f987f61e3a8e46
Maksim Gorokhovskiy <magor1744@gmail.com>
2021-06-09 23:14:19 +0300

    faster prime count

--- prime_count.cpp
+++ prime_count.cpp
@@ -1,48 +1,97 @@
-// works in <1s for 1e11, 4s for 1e12
-ll count_primes(ll n) {
-    auto f = [&](ll n) {
-        // should be multiplicative (f(ab) = f(a)f(b)),
-        // the result will be sum f(p) over all primes
-        return 1;
-    };
-    auto pref = [&](ll n) {
-        // should return sum_{i=1..n} f(i)
-        return n;
+// works in 250ms for n=1e11, in ~1s for n=1e12
+// O(n^2/3 logn)
+// just call count_primes(n)
+struct _count_primes_struct_t_ {
+    vector<int> primes;
+    vector<int> mnprimes;
+    ll ans;
+    ll y;
+    vector<pair<pair<ll, int>, char>> queries;
+
+    void phi(ll n, int a, int sign = 1) {
+        if (n == 0) return;
+        if (a == -1) {
+            ans += n * sign;
+            return;
+        }
+        if (n <= y) {
+            queries.emplace_back(make_pair(n, a), sign);
+            return;
+        }
+        phi(n, a - 1, sign);
+        phi(n / primes[a], a - 1, -sign);
+    }
+
+    struct fenwick {
+        vector<int> tree;
+        int n;
+
+        fenwick(int n = 0) : n(n) {
+            tree.assign(n, 0);
+        }
+
+        void add(int i, int k) {
+            for (; i < n; i = (i | (i + 1)))
+                tree[i] += k;
+        }
+
+        int ask(int r) {
+            int res = 0;
+            for (; r >= 0; r = (r & (r + 1)) - 1)
+                res += tree[r];
+            return res;
+        }
     };
 
-    vector<ll> v;
-    v.reserve((int)sqrt(n) * 2 + 20);
-    ll sq;
-    {
-        ll k = 1;
-        for (; k * k <= n; ++k) {
-            v.pb(k);
-        }
-        --k;
-        sq = k;
-        if (k * k == n) --k;
-        for (; k >= 1; --k) {
-            v.pb(n / k);
-        }
-    }
-    vector<ll> s(v.size());
-    for (int i = 0; i < s.size(); ++i)
-        s[i] = pref(v[i]) - 1;
-    auto geti = [&](ll x) {
-        if (x <= sq) return (int)x - 1;
-        else         return (int)(v.size() - (n / x));
-    };
-    for (ll p = 2; p * p <= n; ++p) {
-        if (s[p - 1] != s[p - 2]) {
-            ll sp = s[p - 2];
-            ll p2 = p * p;
-            for (int i = (int)v.size() - 1; i >= 0; --i) {
-                if (v[i] < p2) {
-                    break;
-                }
-                s[i] -= (s[geti(v[i] / p)] - sp) * f(p);
+    ll count_primes(ll n) {
+        y = pow(n, 0.64);
+        if (n < 100) y = n;
+        primes.clear();
+        mnprimes.assign(y + 1, -1);
+        ans = 0;
+        for (int i = 2; i <= y; ++i) {
+            if (mnprimes[i] == -1) {
+                mnprimes[i] = primes.size();
+                primes.pb(i);
+            }
+            for (int k = 0; k < primes.size(); ++k) {
+                int j = primes[k];
+                if (i * j > y) break;
+                mnprimes[i * j] = k;
+                if (i % j == 0) break;
             }
         }
+        if (n < 100) return primes.size();
+        ll s = n / y;
+        for (int p : primes) {
+            if (p > s) break;
+            ans++;
+        }
+        int ssz = ans;
+        int ptr = primes.size() - 1;
+        for (int i = ssz; i < primes.size(); ++i) {
+            while (ptr >= i && (ll)primes[i] * primes[ptr] > n)
+                --ptr;
+            if (ptr < i) break;
+            ans -= ptr - i + 1;
+        }
+        phi(n, ssz - 1);
+        sort(queries.begin(), queries.end());
+        int ind = 2;
+        int sz = primes.size();
+        // the order will be reversed, because prefix sum in a fenwick is just one query
+        fenwick fw(sz);
+        for (auto [na, sign] : queries) {
+            auto [n, a] = na;
+            while (ind <= n)
+                fw.add(sz - 1 - mnprimes[ind++], 1);
+            ans += (fw.ask(sz - a - 2) + 1) * sign;
+        }
+        queries.clear();
+        return ans - 1;
     }
-    return s.back();
+} _count_primes_struct_;
+
+ll count_primes(ll n) {
+    return _count_primes_struct_.count_primes(n);
 }
