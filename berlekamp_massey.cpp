5789118b07e29c38edffe198b9e1775133ef2992
Maksim Gorokhovskiy <magor1744@gmail.com>
2021-06-12 11:59:53 +0300

    added ntt in berlekamp massey

--- berlekamp_massey.cpp
+++ berlekamp_massey.cpp
@@ -2,12 +2,14 @@
 // m = size of sequence given to find_recurrence()
 
 // find_recurrence() works in O(mn)
-// nth(ind) works O(n^2 log(ind))
+// nth(ind) works in O(n^2*log(ind)) or in O(n*logn*log(ind)) if ntt is enabled
 
 // it is possible to use nth() with precalculated recurrence,
 // use set_recurrence and set_base for that
 
 // rec should satisfy a[i] = sum_{j=0..rec.size()-1} a[i-j-1] * rec[j]
+
+// #define BERLEKAMP_USE_NTT
 
 template<typename T>
 struct BerlekampMassey {
@@ -80,7 +82,67 @@ struct BerlekampMassey {
         find_recurrence(vector<T>(v.begin(), v.end()));
     }
 
-    T nth(long long ind) const {
+    #ifdef BERLEKAMP_USE_NTT
+    T nth_fast(long long ind) const {
+        if (ind < base.size())
+            return base[ind];
+
+        // remove this assert is your mod is different but works fine with ntt
+        static_assert(T::mod() == 998244353, "make sure your mod is correct");
+
+        assert(base.size() == rec.size());
+
+        vector<T> cur_poly = {0, 1};
+        vector<T> res = {1};
+
+        vector<Mint> rec_inv, rec_with_1;
+        int fsize = rec.size() + 1;
+        int N = 1;
+        {
+            while (N < (rec.size() + 1) * 4)
+                N *= 2;
+
+            rec_with_1 = rec;
+            rec_with_1.insert(rec_with_1.begin(), -1);
+            for (auto &k : rec_with_1)
+                k = -k;
+
+            rec_inv = rec_with_1;
+            reverse(rec_with_1.begin(), rec_with_1.end());
+            rec_inv = poly_inv(rec_inv, rec_inv.size());
+
+            rec_inv = ntt::ntt(rec_inv, N);
+            rec_with_1 = ntt::ntt(rec_with_1, N / 2);
+        }
+
+        while (ind) {
+            int cur_poly_sz = cur_poly.size();
+            cur_poly = ntt::ntt(cur_poly, N / 2);
+            if (ind & 1) {
+                int res_sz = res.size();
+                res = ntt::ntt(res, N / 2);
+                for (int i = 0; i < N / 2; ++i)
+                    res[i] *= cur_poly[i];
+                res = ntt::ntti(res);
+                res.resize(cur_poly_sz + res_sz - 1);
+                poly_mod_fast(res, rec_with_1, rec_inv, fsize, N);
+            }
+            for (int i = 0; i < N / 2; ++i)
+                cur_poly[i] *= cur_poly[i];
+            cur_poly = ntt::ntti(cur_poly);
+            cur_poly.resize(cur_poly_sz * 2 - 1);
+            poly_mod_fast(cur_poly, rec_with_1, rec_inv, fsize, N);
+            ind >>= 1;
+        }
+
+        T answer = 0;
+        for (int i = 0; i < res.size(); ++i)
+            answer += base[i] * res[i];
+        return answer;
+    }
+    #endif
+
+    T nth_slow(long long ind) const {
         if (ind < base.size())
             return base[ind];
 
@@ -88,6 +150,7 @@ struct BerlekampMassey {
 
         vector<T> cur_poly = {0, 1};
         vector<T> res = {1};
+
         while (ind) {
             if (ind & 1) {
                 res = poly_mul(res, cur_poly);
@@ -104,6 +167,15 @@ struct BerlekampMassey {
         return answer;
     }
 
+    T nth(long long ind) const {
+        #ifdef BERLEKAMP_USE_NTT
+        if (rec.size() > 150)
+            return nth_fast(ind);
+        #endif
+
+        return nth_slow(ind);
+    }
+
   private:
     vector<T> poly_mul(const vector<T> &a, const vector<T> &b) const {
         vector<T> res(a.size() + b.size() - 1);
@@ -113,7 +185,59 @@ struct BerlekampMassey {
         return res;
     }
 
-    vector<T> poly_mod(vector<T> &a, const vector<T> &f) const {
+    #ifdef BERLEKAMP_USE_NTT
+    void poly_mod_fast(vector<T> &a, const vector<T> &f, const vector<T> &fi, int fsize, int N) const {
+        if (a.size() < fsize) return;
+
+        reverse(a.begin(), a.end());
+        auto d = a;
+        d = ntt::ntt(d, N);
+        for (int i = 0; i < N; ++i)
+            d[i] *= fi[i];
+        d = ntt::ntti(d);
+
+        if (d.size() > a.size() - fsize + 1)
+            d.resize(a.size() - fsize + 1);
+
+        reverse(d.begin(), d.end());
+        reverse(a.begin(), a.end());
+
+        d = ntt::ntt(d, N / 2);
+        for (int i = 0; i < N / 2; ++i)
+            d[i] *= f[i];
+        d = ntt::ntti(d);
+
+        for (int i = 0; i < min(a.size(), d.size()); ++i)
+            a[i] -= d[i];
+
+        a.resize(fsize - 1);
+    }
+
+    vector<T> poly_inv(const vector<T> &v, int k = -1) const {
+        if (k == -1) k = v.size();
+        assert(v[0] != 0);
+        vector<T> res = {T(1) / v[0]};
+        int cur_size = 1;
+        vector<T> v_pref;
+        while (res.size() < k) {
+            cur_size *= 2;
+            while (v_pref.size() < v.size() && v_pref.size() < cur_size)
+                v_pref.pb(v[v_pref.size()]);
+            auto tmp = ntt::multiply(v_pref, res);
+            tmp[0] -= 2;
+            if (tmp.size() > cur_size)
+                tmp.resize(cur_size);
+            for (auto &k : tmp)
+                k = -k;
+            res = ntt::multiply(res, tmp);
+            if (res.size() > cur_size)
+                res.resize(cur_size);
+        }
+        return res;
+    }
+    #endif
+
+    void poly_mod(vector<T> &a, const vector<T> &f) const {
         while (a.size() > f.size()) {
             T k = a.back();
             a.pop_back();
@@ -122,6 +246,5 @@ struct BerlekampMassey {
                     a[a.size() - i - 1] += f[i] * k;
             }
         }
-        return a;
     }
 };
