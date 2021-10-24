8aac6b97ba3448d1d1249fb80710396bc813c838
Maksim Gorokhovskiy <magor1744@gmail.com>
2021-10-24 16:43:42 +0300

    faster get_prime_divs

--- sieve.cpp
+++ sieve.cpp
@@ -1,7 +1,67 @@
 struct Sieve {
+    struct PrimeIterator {
+        int prime;
+        int count;
+        int num;
+        const Sieve& sieve;
+
+        PrimeIterator(int num, const Sieve& sieve) : num(num), sieve(sieve) {
+            step();
+        }
+
+        void step() {
+            show(num);
+            prime = sieve.mn[num];
+            count = 0;
+            while (num != 1 && sieve.mn[num] == prime) {
+                show(num, sieve.mn[num]);
+                num /= sieve.mn[num];
+                ++count;
+            }
+        }
+
+        bool operator == (const PrimeIterator& other) const {
+            return tie(prime, count, num) == tie(other.prime, other.count, other.num);
+        }
+        bool operator != (const PrimeIterator& other) const {
+            return !(*this == other);
+        }
+
+        PrimeIterator& operator ++ () {
+            step();
+            return *this;
+        }
+
+        pair<int, int> operator * () const {
+            return {prime, count};
+        }
+    };
+
+    struct PrimeIteratorContainer {
+        int num;
+        const Sieve& sieve;
+
+        PrimeIteratorContainer(int num, const Sieve& sieve) : num(num), sieve(sieve) {}
+
+        PrimeIterator begin() const {
+            return PrimeIterator(num, sieve);
+        }
+
+        PrimeIterator end() const {
+            return PrimeIterator(1, sieve);
+        }
+
+        vector<pair<int, int>> collect() const {
+            vector<pair<int, int>> result;
+            for (auto p : *this)
+                result.push_back(p);
+            return result;
+        }
+    };
+
     vector<bool> isp;
     vector<int> primes;
-    vector<short> mn;
+    vector<int> mn;
 
     Sieve(int n, bool gen_primes = false, bool gen_mn = false) {
         isp.assign(n + 1, true); isp[0] = isp[1] = false;
@@ -17,10 +77,14 @@ struct Sieve {
 
         if (gen_mn) {
             mn.resize(n + 1);
-            for (int i = 2; i * i <= n; ++i)
-                if (isp[i])
-                    for (int j = i * i; j <= n; j += i)
-                        mn[j] = i;
+            for (int i = 2; i <= n; ++i) {
+                if (isp[i]) {
+                    mn[i] = i;
+                    if ((ll)i * i <= n)
+                        for (int j = i * i; j <= n; j += i)
+                            mn[j] = i;
+                }
+            }
         }
     }
 
@@ -32,23 +96,11 @@ struct Sieve {
         return isp[k];
     }
 
-    vector<pair<int, int>> get_prime_divs(int k) const {
-        static vector<int> v;
-        v.clear();
-        while (k != 1 && !isp[k]) {
-            v.pb(mn[k]);
-            k /= mn[k];
-        }
-        if (k != 1)
-            v.pb(k);
-        sort(v.begin(), v.end());
-        vector<pair<int, int>> res;
-        for (int k : v) {
-            if (res.empty() || k != res.back().first)
-                res.eb(k, 0);
-            res.back().second++;
-        }
-        return res;
+    // can be used as vector<pair<int, int>>, as in function phi() below
+    // except doesn't create a vector to avoid heap allocations
+    // can be transformed into vector<pair<int, int>> with .collect()
+    PrimeIteratorContainer get_prime_divs(int k) const {
+        return PrimeIteratorContainer(k, *this);
     }
 
     int phi(int n) const {
